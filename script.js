dd5575e874c4634f525aaeedc713559ed6d3e8c5
Laka Nodia <laka.nodia@gmail.com>
2022-06-13 23:18:50 +0400

    Add first page responsive and burger bar

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+// burger bar
+let navigation = document.getElementById('nav-block');
+let burgerButton = document.getElementById('burger-bar');
+let topChild = document.getElementById('top-child');
+let middleChild = document.getElementById('middle-child');
+let bottomChild = document.getElementById('bottom-child');
+
+burgerButton.addEventListener('click', function(){
+    navigation.classList.toggle('activeNavigation');
+    topChild.classList.toggle('top');
+    middleChild.classList.toggle('middle');
+    bottomChild.classList.toggle('bottom');
+});
