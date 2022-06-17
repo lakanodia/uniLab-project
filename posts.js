6ed8ecd5af37de0d7d6062ec46e7190f581a853d
Laka Nodia <laka.nodia@gmail.com>
2022-06-17 21:33:31 +0400

    Add only posts

--- posts.js
+++ posts.js
@@ -21,7 +21,7 @@ serverRequest('https://jsonplaceholder.typicode.com/posts', function(data){
 });
 
 function printPosts(data) {
-    data.slice(0,4).forEach(element => {
+    data.slice(0,6).forEach(element => {
         createPosts(element);             
     });
 }
@@ -94,74 +94,3 @@ function postCardInfo(item){
         postContent.innerHTML = '';
     });
 }
-
-
-// xml http request: 
-let currentPage = 1;
-let totalPage;
-
-function getUsers(page){
-    let requist = new XMLHttpRequest();
-    requist.addEventListener('load' , render);
-    requist.addEventListener('error' , errorRender);
-
-    requist.open('GET', 'https://reqres.in/api/users?page=' + page);
-
-    requist.send();
-}
-
-function render(){
-    let response = this.responseText;
-    let responseData = JSON.parse(response);
-    let fragment = document.createDocumentFragment();
-
-    responseData.data.forEach(Element => {
-        let li = document.createElement('li');
-        let pEmail = document.createElement('p');
-        pEmail.textContent = Element.email;
-        pEmail.classList.add('p-Email');
-        let userImg = document.createElement('img');
-        userImg.classList.add('user-img');
-        userImg.src = Element.avatar;
-        li.appendChild(userImg);
-        li.appendChild(pEmail);
-
-        fragment.appendChild(li);
-
-    });
-    totalPage = responseData.total_pages;
-    document.getElementById('user_ul').innerHTML = ' ';
-    document.getElementById('user_ul').appendChild(fragment);
-
-}
-
-function errorRender(error){
-    if(error == 404){
-        let ptext = document.createElement('p');
-        ptext.textContent = 'Server Error';
-        ptext.classList.add('p-text');
-        document.getElementById('user_div').appendChild(ptext);
-    }
-    let perror = document.createElement('p');
-    perror.textContent = 'Page Is Not Found';
-    perror.classList.add('p-text');
-    document.getElementById('user_div').appendChild(perror);
-};
-
-document.getElementById('next').addEventListener('click' , function() {
-    if(currentPage == totalPage){
-        return;
-    }
-    currentPage ++;
-    getUsers(currentPage);
-});
-
-document.getElementById('prev').addEventListener('click' , function() {
-    if(currentPage == 1){
-        return;
-    }
-    currentPage --;
-    getUsers(currentPage);
-})
-
-getUsers(currentPage);
